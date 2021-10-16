317e762707151a4fb0035c02876fe2c894aead7b
MS269 <ehddnr9797@gmail.com>
2021-10-16 22:36:58 +0900

    Commit Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-function App() {
-  return <h1>App</h1>;
-}
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import routes from "./routes";
+import Admin from "./screens/Admin";
+import Bakery from "./screens/Bakery";
+import Chat from "./screens/Chat";
+import Home from "./screens/Home";
+import Manual from "./screens/Manual";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path={routes.home} exact>
+          <Home />
+        </Route>
+        <Route path={routes.admin} exact>
+          <Admin />
+        </Route>
+        <Route path={routes.bakery} exact>
+          <Bakery />
+        </Route>
+        <Route path={routes.chat} exact>
+          <Chat />
+        </Route>
+        <Route path={routes.manual} exact>
+          <Manual />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+const routes: { [key: string]: string } = {
+  home: "/",
+  admin: "/admin",
+  bakery: "/bakery",
+  chat: "/chat",
+  manual: "/manual",
+};
+
+export default routes;
