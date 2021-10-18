100f679757e9757e93467595b3b0948929f8748d
MS269 <ehddnr9797@gmail.com>
2021-10-18 11:33:47 +0900

    Fix Chat Message Bug

--- src/screens/Chat.tsx
+++ src/screens/Chat.tsx
@@ -56,6 +56,7 @@ export default function Chat() {
         <input
           type="text"
           placeholder="메세지"
+          value={newMessage}
           onChange={(event) => setNewMessage(event.target.value)}
         />
         <button onClick={sendMessage}>전송</button>
