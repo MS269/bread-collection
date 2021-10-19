cf2b34f77c9673ab2c1262bcb0f2904bc7ea291e
MS269 <ehddnr9797@gmail.com>
2021-10-19 11:48:50 +0900

    Commit D-Day Counter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,16 @@
 export default function Footer() {
-  return <footer>김포푸드뱅크 ☏ 031-983-1377</footer>;
+  const dischargeDay = new Date(2022, 10, 17);
+  const today = new Date();
+  const dDay: number =
+    Math.floor(
+      (dischargeDay.getTime() - today.getTime()) / 1000 / 60 / 60 / 24
+    ) + 1;
+
+  return (
+    <footer>
+      <span>
+        김포푸드뱅크 ☏ 031-983-1377 <strong>소집해제 D-{dDay}</strong>
+      </span>
+    </footer>
+  );
 }
