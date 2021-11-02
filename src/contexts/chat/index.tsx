eb57da3a98b05a80fa9542de5082ea6b52125562
MS269 <ehddnr9797@gmail.com>
2021-11-02 16:10:03 +0900

    Update Notification

--- src/contexts/chat/index.tsx
+++ src/contexts/chat/index.tsx
@@ -1,12 +1,26 @@
-import React, { createContext, useState } from "react";
+import { collection, onSnapshot, query, where } from "@firebase/firestore";
+import React, { createContext, useEffect, useState } from "react";
+import { db } from "../../firebase";
 
 const ChatContext = createContext({
   count: 0,
-  setCount: (value: number) => {},
+  setCount: (count: number) => {},
 });
 
 function ChatProvider({ children }: { children: React.ReactNode }) {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "chat"), where("read", "==", false)),
+      (snapshot) => {
+        let cnt = 0;
+        snapshot.docs.map(() => cnt++);
+        setCount(cnt);
+      }
+    );
+    return unsubscribe;
+  }, []);
 
   return (
     <ChatContext.Provider value={{ count, setCount }}>
